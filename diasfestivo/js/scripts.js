ecdbf78c3d4d5844d2458258c3cdab762ccfbeda
wilberrivas4 <wilberrivas60@gmail.com>
2016-06-12 07:12:05 -0600

    APP

---
+++ diasfestivo/js/scripts.js
@@ -0,0 +1,27 @@
+
+$(document).ready(function(){/* google maps -----------------------------------------------------*/
+google.maps.event.addDomListener(window, 'load', initialize);
+
+function initialize() {
+
+  /* position Amsterdam */
+  var latlng = new google.maps.LatLng(13.4886147,-88.193072,19);
+
+  var mapOptions = {
+    center: latlng,
+    scrollWheel: false,
+    zoom: 16
+  };
+  
+  var marker = new google.maps.Marker({
+    position: latlng,
+    url: '/',
+    animation: google.maps.Animation.DROP
+  });
+  
+  var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+  marker.setMap(map);
+
+};
+/* end google maps -----------------------------------------------------*/
+});
